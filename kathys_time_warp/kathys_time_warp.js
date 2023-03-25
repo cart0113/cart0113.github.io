5b769fe3fadc8a29f4fc71018fa8eb27d52eb4ef
Andrew J. Gjevre Carter <andrewjcarter@gmail.com>
2023-03-25 12:27:34 -0500

    TIMEWARP

--- kathys_time_warp/kathys_time_warp.js
+++ kathys_time_warp/kathys_time_warp.js
@@ -1,3 +1,8 @@
+const soundEffect = new Audio();
+soundEffect.autoplay = true;
+soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+
+
 const containerEl = document.getElementById('countdown-container');
 const countdownEl = document.getElementById('countdown-canvas');
 
@@ -17,7 +22,9 @@ const countdown = new CanvasCircularCountdown(countdownEl, {
   (percentage, time, instance) => {
       if (percentage == 0) {
         //https://stackoverflow.com/questions/21815323/have-sound-play-when-alert-is-triggered
-        document.getElementById('xyz').play();      
+        //document.getElementById('xyz').play(); 
+        soundEffect.src = "alarm.mp3";
+        soundEffect.play();
       }
    }
 ).start();
